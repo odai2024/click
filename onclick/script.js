a79b2cdde317cfb6ca77fb52a960c967e4046b90
odai2024 <odai2015100@gmail.com>
2024-09-15 19:05:59 +0300

    Add files via upload

---
+++ onclick/script.js
@@ -0,0 +1,9 @@
+function odai(){
+document.getElementById("text").innerHTML="marwan";
+}
+function ahmed(){
+document.getElementById("text2").innerHTML="mohamad";
+}
+function roblox(){ 
+document.getElementById("text3").innerHTML="minecraft";
+}
