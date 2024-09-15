4d2b6dd9efc6829ec6a7c534ff12abf73f5efc1a
odai2024 <odai2015100@gmail.com>
2024-09-15 19:10:14 +0300

    Delete onclick directory

--- onclick/script.js
+++
@@ -1,9 +0,0 @@
-function odai(){
-document.getElementById("text").innerHTML="marwan";
-}
-function ahmed(){
-document.getElementById("text2").innerHTML="mohamad";
-}
-function roblox(){ 
-document.getElementById("text3").innerHTML="minecraft";
-}
